a6615af32f0a7e9ca99675b54025f1cca8fbc5ef
@BrendleCenido <cenido.brendle@gmail.com>
2022-03-12 13:16:29 +0800

    DONE ACTIVITY

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,82 +1,77 @@
 import React from "react";
-import { View, Image, Text, Dimensions } from "react-native";
+import {StyleSheet,View,Text, TextInput,TouchableOpacity, Image} from  "react-native";
 
 export default function TabOneScreen() {
   return (
-    <View
-      style={{
-        flex: 1,
-        backgroundColor: '#273746',
-        alignItems: 'center',
-        justifyContent: 'center'
-      }}
-    >
-      <View style={{
-        height: Dimensions.get('screen').height * 0.5,
-        width: '80%',
-        backgroundColor: 'white',
-        padding: 10,
-        borderRadius: 10
-      }}>
-        <View
-          style={{
-            flex: 1,
-            backgroundColor: 'red',
-            marginBottom: 10,
-            borderRadius: 10,
-            overflow: 'hidden'
-          }}
-        >
-          <Image
-            style={{
-              height: '100%',
-              width: '100%',
-              resizeMode: 'cover'
-            }}
-            source={require('../assets/images/1099445.png')}
-          />
-        </View>
-        <View
-          style={{
-            flex: 0,
-            backgroundColor: '#E9DBD8',
-            paddingVertical: 10,
-            paddingHorizontal: 10,
-            marginBottom: 10,
-            borderRadius: 10,
-          }}
-        >
-          <Text
-            style={{
-              color: '#877D7B',
-              fontFamily: 'poppins-bold',
-              fontSize: 18,
-            }}
-            numberOfLines={1}
-          >
-            My NFT Card
-          </Text>
-        </View>
-        <View
-          style={{
-            flex: 0,
-            minHeight: 100,
-            maxHeight: 200,
-            backgroundColor: 'white',
-            padding: 5,
-          }}
-        >
-          <Text
-            style={{
-              fontFamily: 'poppins-regular',
-              color: '#877D7B',
-              fontSize: 14
-            }}
-          >
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-          </Text>
-        </View>
-      </View>
+    <View style={styles.container}>
+      <Image source = {require('C:/Users/Brendle Cenido/OneDrive/Documents/BSIT 2-1A (2nd Sem)/APPLICATION DEV/PROJECTS/sample-test-kit/assets/images/LOGIN.png')} 
+      style={styles.imageIcon}/>
+      <Text style={styles.Login}>Welcome!</Text>
+      <TextInput style={styles.inputText}
+        placeholder= "Enter Username"
+      />
+      <TextInput style={styles.inputText}
+        placeholder= "Enter Password"
+        secureTextEntry
+      />
+      <TouchableOpacity style={styles.btnLogin}>
+        <Text style={styles.btnText}>Login</Text>
+      </TouchableOpacity>
+      <Text style={styles.basictext}>No Account yet?</Text>
+      <TouchableOpacity style={styles.btnSignUp}>
+        <Text style={styles.btnText}>Sign-up</Text>
+      </TouchableOpacity>
     </View>
-  );
+  )
 }
+
+const styles = StyleSheet.create({
+container: {
+  flex: 1,
+  backgroundColor: '#656D4A',
+  justifyContent: 'center',
+  alignItems: 'center',
+  fontFamily: 'poppins-regular'
+},
+Login: {
+ fontSize: 30,
+  textAlign: 'center',
+  margin: 18,
+  fontFamily: 'poppins-bold',
+  color: 'white'
+},
+inputText:{
+  width: "90%",
+  backgroundColor:'#A4AC86',
+  padding: 15,
+  marginBottom: 10,
+  color: '#333D29'
+},
+btnLogin: {
+  backgroundColor: '#582F0E',
+  padding: 15,
+  width: '45%',
+  justifyContent: 'center',
+  marginBottom: 30
+},
+btnSignUp: {
+  backgroundColor: '#582F0E',
+  padding: 15,
+  width: '30%',
+  justifyContent: 'center'
+},
+btnText: {
+  fontSize: 16,
+  textAlign: 'center',
+  color: '#B6AD90',
+  fontFamily: 'poppins-bold'
+},
+imageIcon: {     
+  width: 100, 
+  height: 100 
+},
+basictext: {
+  fontFamily: 'poppins-semi-bold'
+}
+});
+  
