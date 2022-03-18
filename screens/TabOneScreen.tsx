a658d53f85f3087b8b6b50d83fe3888fc3757321
@BrendleCenido <cenido.brendle@gmail.com>
2022-03-18 20:46:09 +0800

    DONE ACTIVITY

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,77 +1,187 @@
-import React from "react";
-import {StyleSheet,View,Text, TextInput,TouchableOpacity, Image} from  "react-native";
+import React, { useState } from "react";
+import { View, Image, Text, Dimensions, StyleSheet, Alert, Keyboard, KeyboardAvoidingView,  } from "react-native";
+import {Button, colors} from "react-native-elements";
+import LottieView from 'lottie-react-native';
+import { TextInput } from 'react-native-paper';
+
 
 export default function TabOneScreen() {
+  const [email, setEmail] = useState<string>("");
+  const [password, setPasssword] = useState<string>("");
+  const [visible, setVisible] = useState<boolean>(true);
+  const [loading, setLoading] = useState<boolean>(false); 
+
+  const handleLogin = () => {
+    const EMAIL_ADDRESS = "cenido.brendle@gmail.com";
+    const PASSWORD = "12345";
+    setLoading(true);
+
+    setTimeout(() => {
+      setLoading(false);
+      if (email === EMAIL_ADDRESS && password === PASSWORD){ 
+        return Alert.alert(
+          "LOG IN",
+          'Successfully logged in!'
+        )
+      }
+  
+      Alert.alert(
+        "INVALID CREDENTIALS",
+        "Please enter a correct username and password",
+      )
+    }, 500);
+  }
+
   return (
-    <View style={styles.container}>
-      <Image source = {{uri:'https://cdn-icons-png.flaticon.com/512/4128/4128176.png'}}
-      style={styles.imageIcon}/>
-      <Text style={styles.Login}>Welcome!</Text>
-      <TextInput style={styles.inputText}
-        placeholder= "Enter Username"
-      />
-      <TextInput style={styles.inputText}
-        placeholder= "Enter Password"
-        secureTextEntry
-      />
-      <TouchableOpacity style={styles.btnLogin}>
-        <Text style={styles.btnText}>Login</Text>
-      </TouchableOpacity>
-      <Text style={styles.basictext}>No Account yet?</Text>
-      <TouchableOpacity style={styles.btnSignUp}>
-        <Text style={styles.btnText}>Sign-up</Text>
-      </TouchableOpacity>
+    <View
+      style={styles.Container}
+    >
+      <View style={styles.Lottielogo}>
+        <LottieView
+          source={require('../assets/images/72342-welcome.json')}
+          autoPlay={true}
+          loop={true}
+        />
+      </View>
+
+      <View style={{
+        flex: 1,
+        top: -80
+      }}>
+        <View style={{
+          flex: 0,
+          marginBottom: 20
+        }}>
+          <Text style={styles.Login}>Welcome!</Text>
+          <TextInput
+            label = "Email"
+            value={email}
+            onChangeText={text => setEmail(text)}
+            autoComplete = {false}
+            autoCapitalize = {"none"}
+            keyboardType = {"email-address"}
+            mode = {"flat"}
+            right = {
+              <TextInput.Icon 
+              name="email" 
+              size={24} 
+              color="black"
+              disabled = {true}
+              />
+            }
+            theme={{
+              colors: {
+                placeholder: '#293241',
+                primary: '#293241',
+                background: '#E0FBFC'
+              }
+            }}
+        />
+        </View>
+
+        <View style={{
+          flex: 0,
+          marginBottom: 40
+        }}>
+          <TextInput
+            label = "Password" 
+            value={password}
+            onChangeText={text => setPasssword(text)}
+            autoComplete = {false}
+            mode = {"flat"}
+            right={
+            <TextInput.Icon 
+            name= {visible ? "eye-off" : "eye"}
+            color = {"#3D5A80"}
+            onPress={() => {
+              setVisible(!visible)
+            }}
+            />}
+            secureTextEntry={visible}
+            theme={{
+              colors: {
+                placeholder: '#293241',
+                primary: '#293241',
+                background: '#E0FBFC'
+              }
+            }}
+        />
+        </View>
+
+        <View style={{
+          flex: 0,
+        }}>
+
+          <View style={{
+            flex: 0,
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}>
+            <Button
+              title="LOG IN"
+              buttonStyle={{
+                backgroundColor: '#EF795F',
+                borderWidth: 2,
+                borderColor: '#293241',
+                borderRadius: 30,
+              }}
+              containerStyle={{
+                width: 200,
+                marginHorizontal: 50,
+                marginVertical: 10,
+                marginBottom: 30,
+              }}
+              titleStyle={{ fontWeight: 'bold',
+               color: '#e0fbfc'
+              }}
+              onPress = {handleLogin}
+              loading = {loading}
+            />
+          </View>
+
+        </View>
+      </View>
+
+      <View style={{
+        height: 50,
+        width: '100%',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 10,
+        paddingBottom: 20,
+      }}>
+        <Text style={styles.Signup}>
+          No account yet?
+        </Text>
+      </View>
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
-container: {
-  flex: 1,
-  backgroundColor: '#656D4A',
-  justifyContent: 'center',
-  alignItems: 'center',
-  fontFamily: 'poppins-regular'
-},
-Login: {
- fontSize: 30,
-  textAlign: 'center',
-  margin: 18,
-  fontFamily: 'poppins-bold',
-  color: 'white'
-},
-inputText:{
-  width: "90%",
-  backgroundColor:'#A4AC86',
-  padding: 15,
-  marginBottom: 10,
-  color: '#333D29'
-},
-btnLogin: {
-  backgroundColor: '#582F0E',
-  padding: 15,
-  width: '45%',
-  justifyContent: 'center',
-  marginBottom: 30
-},
-btnSignUp: {
-  backgroundColor: '#582F0E',
-  padding: 15,
-  width: '30%',
-  justifyContent: 'center'
-},
-btnText: {
-  fontSize: 16,
-  textAlign: 'center',
-  color: '#B6AD90',
-  fontFamily: 'poppins-bold'
-},
-imageIcon: {     
-  width: 100, 
-  height: 100 
-},
-basictext: {
-  fontFamily: 'poppins-semi-bold'
-}
+  Container: {
+    flex: 1,
+    backgroundColor: '#98C1D9',
+    padding: 20
+  },
+  Login: {
+     fontSize: 30,
+      textAlign: 'center',
+      margin: 18,
+      fontFamily: 'poppins-bold',
+      color: 'black'
+  },
+  Signup: {
+    fontFamily: 'poppins-regular',
+    fontSize: 20,
+    top: -20
+  },
+  Lottielogo: {
+    flex: 1,
+    height: '100%',
+    width: '100%',
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: -20
+  }
 });
-  
