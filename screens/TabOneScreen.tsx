83031a7f4911572325d6572b9e8cb04ae9db0001
@BrendleCenido <cenido.brendle@gmail.com>
2022-04-02 13:55:50 +0800

    done activity

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,9 +1,13 @@
 import React, { useState } from "react";
-import { View, Image, Text, Dimensions, StyleSheet, Alert, Keyboard, KeyboardAvoidingView} from "react-native";
+import { View, Image, Text, Dimensions, StyleSheet, Alert, Keyboard, KeyboardAvoidingView, ScrollView} from "react-native";
 import {Button, colors} from "react-native-elements";
 import LottieView from 'lottie-react-native';
 import { TextInput } from 'react-native-paper';
 import { Ionicons } from '@expo/vector-icons';
+import { Formik } from "formik";
+import * as yup from 'yup';
+import ViewWithLoading from "../components/ViewWithLoading";
+import { isLoading } from "expo-font";
 
 
 export default function TabOneScreen() {
@@ -15,229 +19,236 @@ export default function TabOneScreen() {
   const [visible, setVisible] = useState<boolean>(true);
 
 
-  return (
-    <KeyboardAvoidingView
-      style={styles.Container}
-      behavior="padding"
+const registerSchema = yup.object({
+  email: yup.string().required('Email is required').matches(/htrhtjytjyt7/, 'Invalid email'),
+  password: yup.string().required('Password is required')
+});
+
+return (
+  <ViewWithLoading loading={loading}>
+    <Formik 
+      initialValues={{
+      email: '',
+      password: ''
+      }}
+      onSubmit={(values, action) => {
+        console.log(values);
+      }}
+      validationSchema={registerSchema}
     >
-      <View style={{
-        marginTop: 10
-      }}>
-        <Button
-              buttonStyle={{
-                backgroundColor: '#98C1D9'
-              }}
-              containerStyle={{
-                width: 100,
-                marginHorizontal: -30,
-                marginVertical: 10,
-                marginBottom: 10,
-              }}
-              icon = {<Ionicons 
-                name="chevron-back-circle-outline" 
-                size={50} 
-                color="black"
-              />}
-          />
+      { ({handleChange, values, errors, touched, handleSubmit}) => (
+            <View
+              style={styles.Container}
+            >
+              <View style={{
+                marginTop: 10
+              }}>
+                <Button
+                      buttonStyle={{
+                        backgroundColor: '#98C1D9'
+                      }}
+                      containerStyle={{
+                        width: 100,
+                        marginHorizontal: -30,
+                        marginVertical: 10,
+                        marginBottom: 10,
+                      }}
+                      icon = {<Ionicons 
+                        name="chevron-back-circle-outline" 
+                        size={50} 
+                        color="black"
+                      />}
+                  />
+              </View>
+              <View style={styles.Lottielogo}>
+                <LottieView
+                  source={require('../assets/images/53395-login.json')}
+                  autoPlay={true}
+                  loop={true}
+                />
+              </View>
+              <View style={{
+                flex: 1,
+                top: -140
+              }}>
+                <Text style={styles.Login}>Sign-up</Text>
+            <ScrollView style={{
+                  flex: 1,
+                  marginTop: -100,
+                  top: 100
+            }}>
+                  <View style={{
+                  flex: 0,
+                  marginBottom: 5
+                }}>
+                  <TextInput
+                    label = "First Name" 
+                    value={name}
+                    onChangeText={text => setName(text)}
+                    autoComplete = {false}
+                    mode = {"flat"}
+                    right={
+                    <TextInput.Icon 
+                    name= "pen"
+                    color = {"#3D5A80"}
+                    disabled = {true}
+                    />}
+                    theme={{
+                      colors: {
+                        placeholder: '#293241',
+                        primary: '#293241',
+                        background: '#E0FBFC'
+                      }
+                    }}
+                />
+                </View>
+                <View style={{
+                  flex: 0,
+                  marginBottom: 5
+                }}>
+                  <TextInput
+                    label = "Last Name" 
+                    value={phone}
+                    onChangeText={text => setPhone(text)}
+                    autoComplete = {false}
+                    mode = {"flat"}
+                    right={
+                    <TextInput.Icon 
+                    name= "pen"
+                    color = {"#3D5A80"}
+                    disabled = {true}
+                    />}
+                    theme={{
+                      colors: {
+                        placeholder: '#293241',
+                        primary: '#293241',
+                        background: '#E0FBFC'
+                      }
+                    }}
+                />
+                </View>
         
-      </View>
-      <View style={styles.Lottielogo}>
-        <LottieView
-          source={require('../assets/images/53395-login.json')}
-          autoPlay={true}
-          loop={true}
-        />
-      </View>
-
-      <View style={{
-        flex: 1,
-        top: -140
-      }}>
-        <View style={{
-          flex: 0,
-          marginBottom: 5
-        }}>
-          <Text style={styles.Login}>Sign-up</Text>
-          <View style={{
-          flex: 0,
-          marginBottom: 5
-        }}>
-          <TextInput
-            label = "Full Name" 
-            value={name}
-            onChangeText={text => setName(text)}
-            autoComplete = {false}
-            mode = {"flat"}
-            right={
-            <TextInput.Icon 
-            name= "pen"
-            color = {"#3D5A80"}
-            disabled = {true}
-            />}
-            theme={{
-              colors: {
-                placeholder: '#293241',
-                primary: '#293241',
-                background: '#E0FBFC'
-              }
-            }}
-        />
-        </View>
-
-        <View style={{
-          flex: 0,
-          marginBottom: 5
-        }}>
-          <TextInput
-            label = "Phone number" 
-            value={phone}
-            onChangeText={text => setPhone(text)}
-            autoComplete = {false}
-            mode = {"flat"}
-            right={
-            <TextInput.Icon 
-            name= "phone"
-            color = {"#3D5A80"}
-            disabled = {true}
-            />}
-            theme={{
-              colors: {
-                placeholder: '#293241',
-                primary: '#293241',
-                background: '#E0FBFC'
-              }
-            }}
-        />
-        </View>
-
-          <TextInput
-            label = "E-mail address"
-            value={email}
-            onChangeText={text => setEmail(text)}
-            autoComplete = {false}
-            autoCapitalize = {"none"}
-            keyboardType = {"email-address"}
-            mode = {"flat"}
-            right = {
-              <TextInput.Icon  
-                name="email-edit" 
-                size={24} 
-                color="black" 
-                disabled = {true}
-              />
-            }
-            theme={{
-              colors: {
-                placeholder: '#293241',
-                primary: '#293241',
-                background: '#E0FBFC'
-              }
-            }}
-        />
-        </View>
-
-        <View style={{
-          flex: 0,
-          marginBottom: 5
-        }}>
-          <TextInput
-            label = "Create a Password" 
-            value={password}
-            onChangeText={text => setPassword(text)}
-            autoComplete = {false}
-            mode = {"flat"}
-            right={
-            <TextInput.Icon 
-            name= {visible ? "eye-off" : "eye"}
-            color = {"#3D5A80"}
-            onPress={() => {
-              setVisible(!visible)
-            }}
-            />}
-            secureTextEntry={visible}
-            theme={{
-              colors: {
-                placeholder: '#293241',
-                primary: '#293241',
-                background: '#E0FBFC'
-              }
-            }}
-        />
-        </View>
-
-        <View style={{
-          flex: 0,
-          marginBottom: 40
-        }}>
-          <TextInput
-            label = "Re-enter your password" 
-            value={repassword}
-            onChangeText={text => setRepassword(text)}
-            autoComplete = {false}
-            mode = {"flat"}
-            right={
-            <TextInput.Icon 
-            name= {visible ? "eye-off" : "eye"}
-            color = {"#3D5A80"}
-            onPress={() => {
-              setVisible(!visible)
-            }}
-            />}
-            secureTextEntry={visible}
-            theme={{
-              colors: {
-                placeholder: '#293241',
-                primary: '#293241',
-                background: '#E0FBFC'
-              }
-            }}
-        />
-      </View>
-
-        <View style={{
-          flex: 0,
-        }}>
-
-          <View style={{
-            flex: 0,
-            justifyContent: 'center',
-            alignItems: 'center',
-          }}>
-            <Button
-              title="CREATE ACCOUNT"
-              buttonStyle={{
-                backgroundColor: '#EF795F',
-                borderWidth: 2,
-                borderColor: '#293241',
-                borderRadius: 30,
-              }}
-              containerStyle={{
-                width: 300,
-                marginHorizontal: 50,
-                marginVertical: 10,
-                marginBottom: 30,
-              }}
-              titleStyle={{ fontWeight: 'bold',
-               color: '#e0fbfc'
-              }}
-            />
-          </View>
-
-        </View>
-      </View>
-
-      <View style={{
-        height: 50,
-        width: '100%',
-        justifyContent: 'center',
-        alignItems: 'center',
-        borderRadius: 10,
-        paddingBottom: 20,
-      }}>
-      </View>
-    </KeyboardAvoidingView>
-  );
+                <View style={{
+                  flex: 0,
+                  marginBottom: 5
+                }}>
+                  <TextInput
+                    label = "E-mail address"
+                    value={email}
+                    onChangeText={text => setEmail(text)}
+                    autoComplete = {false}
+                    autoCapitalize = {"none"}
+                    keyboardType = {"email-address"}
+                    mode = {"flat"}
+                    right = {
+                      <TextInput.Icon  
+                        name="email-edit" 
+                        size={24} 
+                        color="black" 
+                        disabled = {true}
+                      />
+                    }
+                    theme={{
+                      colors: {
+                        placeholder: '#293241',
+                        primary: '#293241',
+                        background: '#E0FBFC'
+                      }
+                    }}
+                />
+                </View>
+        
+                <View style={{
+                  flex: 0,
+                  marginBottom: 5
+                }}>
+                  <TextInput
+                    label = "Password" 
+                    value={password}
+                    onChangeText={text => setPassword(text)}
+                    autoComplete = {false}
+                    mode = {"flat"}
+                    right={
+                    <TextInput.Icon 
+                    name= {visible ? "eye-off" : "eye"}
+                    color = {"#3D5A80"}
+                    onPress={() => {
+                      setVisible(!visible)
+                    }}
+                    />}
+                    secureTextEntry={visible}
+                    theme={{
+                      colors: {
+                        placeholder: '#293241',
+                        primary: '#293241',
+                        background: '#E0FBFC'
+                      }
+                    }}
+                />
+                </View>
+        
+                <View style={{
+                  flex: 0,
+                  marginBottom: 5
+                }}>
+                  <TextInput
+                    label = "Confirm Password" 
+                    value={repassword}
+                    onChangeText={text => setRepassword(text)}
+                    autoComplete = {false}
+                    mode = {"flat"}
+                    right={
+                    <TextInput.Icon 
+                    name= {visible ? "eye-off" : "eye"}
+                    color = {"#3D5A80"}
+                    onPress={() => {
+                      setVisible(!visible)
+                    }}
+                    />}
+                    secureTextEntry={visible}
+                    theme={{
+                      colors: {
+                        placeholder: '#293241',
+                        primary: '#293241',
+                        background: '#E0FBFC'
+                      }
+                    }}
+                />
+                </View>
+            </ScrollView>
+        
+                  <View style={{
+                    flex: 0,
+                    alignItems: 'center',
+                    top: 150
+                  }}>
+                    <Button
+                      title="CREATE ACCOUNT"
+                      buttonStyle={{
+                        backgroundColor: '#EF795F',
+                        borderWidth: 2,
+                        borderColor: '#293241',
+                        borderRadius: 30,
+                      }}
+                      containerStyle={{
+                        width: 300,
+                        marginHorizontal: 50,
+                        marginVertical: 10,
+                        marginBottom: 30,
+                      }}
+                      titleStyle={{ fontWeight: 'bold',
+                       color: '#e0fbfc'
+                      }}
+                    />
+                  </View>
+              </View>
+            </View>
+      ) }
+    </Formik>
+  </ViewWithLoading>
+);
 }
+
 
 const styles = StyleSheet.create({
   Container: {
